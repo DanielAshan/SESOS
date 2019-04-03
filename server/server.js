399dd9400463857320d1fef22b58962721bb9b4d
Daniel Pieczyński <pieczynski.daniel@gmail.com>
2019-04-03 22:19:54 +0200

    Connection to database and first migration

--- server/server.js
+++ server/server.js
@@ -1,2 +1,10 @@
-console.log('Hello World! This is SESOS project');
-console.log('Testowa zmiana');
+var express = require('express');
+var server = express();
+var port = 3000;
+server.get('/', function (req, res){
+    res.send('Hello world');
+});
+
+server.listen(port, () => {
+    console.log('Server listening on ' + port)
+});
