974fad520e34a060133b19d60a897be5dc152f79
Daniel Pieczyński <pieczynski.daniel@gmail.com>
2019-04-07 10:19:19 +0200

    Eslint added and configured

--- server/migrations/1554321775418_users.js
+++ server/migrations/1554321775418_users.js
@@ -1,35 +1,35 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-    pgm.createTable("users",
-    {
-        id: "id",
-        username: {
-            type: "varchar(100)",
-            notNull: true,
-        },
-        password: {
-            type: "varchar(100)",
-            notNull: true,
-        },
-        first_name: {
-            type: "varchar(100)",
-            notNull: true,
-        },
-        last_name: {
-            type: "varchar(100)",
-            notNull: true,
-        },
-        email: {
-            type: "varchar(200)",
-            notNull: true,
-        },
-        createdAt: {
-            type: "timestamp",
-            notNull: true,
-            default: pgm.func("current_timestamp")
-          }
-    });
+	pgm.createTable('users',
+		{
+			id: 'id',
+			username: {
+				type: 'varchar(100)',
+				notNull: true,
+			},
+			password: {
+				type: 'varchar(100)',
+				notNull: true,
+			},
+			first_name: {
+				type: 'varchar(100)',
+				notNull: true,
+			},
+			last_name: {
+				type: 'varchar(100)',
+				notNull: true,
+			},
+			email: {
+				type: 'varchar(200)',
+				notNull: true,
+			},
+			createdAt: {
+				type: 'timestamp',
+				notNull: true,
+				default: pgm.func('current_timestamp')
+			}
+		});
 };
 
 exports.down = (pgm) => {
--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var server = express();
 var port = 3000;
 server.get('/', function (req, res){
-    res.send('Hello world');
+	res.send('Hello world');
 });
 
 server.listen(port, () => {
-    console.log('Server listening on ' + port)
+	console.log('Server listening on ' + port);
 });
