d7ef9283b57fe66b17445ff6ca0da3d1d85c43db
Daniel Pieczyński <pieczynski.daniel@gmail.com>
2019-05-05 22:56:49 +0200

    Added vue.js and example components

---
+++ sesos-client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <app-header></app-header>
+      <app-classroom></app-classroom> 
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Shared/Header";
+import ClassroomList from "./components/Classroom/ClassroomList";
+export default {
+  name: "app",
+  components: {
+    "app-header": Header,
+    "app-classroom": ClassroomList
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ sesos-client/src/components/Classroom/ClassroomList.vue
@@ -0,0 +1,44 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(classroom, index) in classrooms" v-bind:key="classroom.name">
+        <th scope="row">{{ index }}</th>
+        <td>{{ classroom.name }}</td>
+        <td>
+          <button type="button" class="btn btn-info">Edit</button>
+          <button type="button" class="btn btn-danger">Remove</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "ClassroomList",
+  data: function() {
+    return {
+      classrooms: [
+        {
+          name: "E3"
+        },
+        {
+          name: "E4"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
+
